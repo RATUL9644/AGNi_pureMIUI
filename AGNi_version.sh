e1456250aa741a80617a4fdab3bcfa27b0d8435f
psndna88 <psndna88@gmail.com>
2023-06-01 00:15:35 +0530

    AGNi haydn AOSP-ST v3.0 beta4

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -3,10 +3,10 @@
 ## AGNi version info
 KERNELDIR=`readlink -f .`
 
-export AGNI_VERSION_PREFIX="beta3"
+export AGNI_VERSION_PREFIX="beta4"
 export AGNI_VERSION="v3.0"
 sed -i 's/5.4.233/5.4.233/' $KERNELDIR/arch/arm64/configs/agni_*
-sed -i 's/v3.0-beta2/v3.0-beta3/' $KERNELDIR/arch/arm64/configs/agni_*
+sed -i 's/v3.0-beta3/v3.0-beta4/' $KERNELDIR/arch/arm64/configs/agni_*
 
 echo "	AGNi Version info loaded."
 
