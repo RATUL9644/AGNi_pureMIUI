35b2ff539586d9a192d81d867814caeaa099b1a0
psndna88 <psndna88@gmail.com>
2023-08-02 11:43:25 +0530

    AGNi AOSP-ST v4.1 stable [5.4.251]

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -4,9 +4,9 @@
 KERNELDIR=`readlink -f .`
 
 export AGNI_VERSION_PREFIX="stable"
-export AGNI_VERSION="v4.0"
+export AGNI_VERSION="v4.1"
 sed -i 's/5.4.250/5.4.251/' $KERNELDIR/arch/arm64/configs/agni_*
-sed -i 's/v3.9.1-stable/v4.0-stable/' $KERNELDIR/arch/arm64/configs/agni_*
+sed -i 's/v4.0-stable/v4.1-stable/' $KERNELDIR/arch/arm64/configs/agni_*
 
 echo "	AGNi Version info loaded."
 
